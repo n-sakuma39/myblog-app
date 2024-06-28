1560d00f887f5dcaf47a629432cf69b24eb3812b
n-sakuma39 <naoto.sakuma39@gmail.com>
2024-06-28 17:43:42 +0900

    【modifiy】all adjust

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,10 +4,12 @@ import styles from "@/styles/app/contact/contact.module.scss";
 const page = () => {
   return (
     <main>
-      <div className={styles.formContainer}>
-        <h2 className={styles.title}>CONTACT</h2>
-        <ContactForm />
-      </div>
+      <section className={styles.section}>
+        <div className={styles.formContainer}>
+          <h2 className={styles.title}>CONTACT</h2>
+          <ContactForm />
+        </div>
+      </section>
     </main>
   );
 };
