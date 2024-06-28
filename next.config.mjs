a79a62bca1d7adeb64b03c587074666583015112
n-sakuma39 <naoto.sakuma39@gmail.com>
2024-06-28 18:42:02 +0900

    【modifiy】all adjust

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.microcms-assets.io"],
+    domains: ["images.microcms-assets.io", "myblog-app-two.vercel.app"],
   },
   env: {
     SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL,
